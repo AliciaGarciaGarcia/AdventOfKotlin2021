631485406c9415cddd3128040bb98bd63dd31e4b
Alicia Garcia <alicia.garcia@liferay.com>
2021-12-16 23:59:48 +0100

    Day 6, first part with two ranges

--- src/main/kotlin/com/aliciagarcia/adventofkotlin2021/Day6.kt
+++ src/main/kotlin/com/aliciagarcia/adventofkotlin2021/Day6.kt
@@ -1,9 +1,6 @@
 package  com.aliciagarcia.adventofkotlin2021
 
 import java.io.File
-import kotlin.math.abs
-import kotlin.math.max
-import kotlin.math.min
 
 fun main() {
     val day6 = Day6()
@@ -18,13 +15,31 @@ class Day6(fileName: String = "day6.txt") {
     private val file = File(ClassLoader.getSystemResource(fileName).file)
 
 
-    val firstGenerationLanternfish = file.readLines().stream().findFirst().get().split(",").map{it.toInt()}
+    var currentGenerationLanternfish = file.readLines().stream().findFirst().get().split(",").map { it.toInt() }
 
-    fun passDaysMortal(days:Int):Int{
+    fun passDaysMortal(days: Int): Int {
 
 
-        return 0;
+        (1..days).forEach { _ ->
+            currentGenerationLanternfish = getNextGeneration()
+        }
 
+        return currentGenerationLanternfish.size
+
+    }
+
+    private fun getNextGeneration(): MutableList<Int> {
+        val _temp: MutableList<Int> = currentGenerationLanternfish.toMutableList()
+
+        (currentGenerationLanternfish.indices).forEach { index ->
+            if (currentGenerationLanternfish[index] == 0) {
+                _temp[index] = 6
+                _temp.add(8)
+            } else {
+                _temp[index] = currentGenerationLanternfish[index] - 1
+            }
+        }
+        return _temp
     }
 }
 
